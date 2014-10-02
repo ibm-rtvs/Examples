15d87dd4a5388c09a7f69b1b9ab5a9f5fff8dc29
ibm-jkelly <john.kelly@uk.ibm.com>
2014-10-02 15:23:56 +0100

    refactor

--- calculator/applications/client/startCalculator.bat
+++ calculator/applications/client/startCalculator.bat
@@ -1,12 +1,6 @@
-rem ***************************************************************************
-@rem Licensed Materials - Property of IBM
-@rem (c) Copyright IBM Corporation 2014. All Rights Reserved.
-@rem
-@rem Note to U.S. Government Users Restricted Rights:  
-@rem Use, duplication or disclosure restricted by GSA ADP Schedule 
-@rem Contract with IBM Corp. 
 @rem ***************************************************************************
-@echo off
+@rem Start Calculator Application 
+@rem ***************************************************************************
 
 pushd
 setlocal
@@ -17,8 +11,8 @@ title Calculator Client
 set AXIS2_HOME=./axis2-1.6.2
 
 
-set CP=-classpath %AXIS2_HOME%/lib/axiom-api-1.2.13.jar;%AXIS2_HOME%/lib/axiom-impl-1.2.13.jar;%AXIS2_HOME%/lib/axis2-adb-1.6.2.jar;%AXIS2_HOME%/lib/axis2-kernel-1.6.2.jar;%AXIS2_HOME%/lib/axis2-transport-http-1.6.2.jar;%AXIS2_HOME%/lib/axis2-transport-local-1.6.2.jar;%AXIS2_HOME%/lib/commons-codec-1.3.jar;%AXIS2_HOME%/lib/commons-httpclient-3.1.jar;%AXIS2_HOME%/lib/commons-logging-1.1.1.jar;%AXIS2_HOME%/lib/httpcore-4.0.jar;%AXIS2_HOME%/lib/mail-1.4.jar;%AXIS2_HOME%/lib/neethi-3.0.2.jar;%AXIS2_HOME%/lib/wsdl4j-1.6.2.jar;%AXIS2_HOME%/lib/XmlSchema-1.4.7.jar
-java %CP% -jar calculator.jar http://localhost:7819/RTCP
+set CP=-classpath ./calculator.jar;%AXIS2_HOME%/lib/axiom-api-1.2.13.jar;%AXIS2_HOME%/lib/axiom-impl-1.2.13.jar;%AXIS2_HOME%/lib/axis2-adb-1.6.2.jar;%AXIS2_HOME%/lib/axis2-kernel-1.6.2.jar;%AXIS2_HOME%/lib/axis2-transport-http-1.6.2.jar;%AXIS2_HOME%/lib/axis2-transport-local-1.6.2.jar;%AXIS2_HOME%/lib/commons-codec-1.3.jar;%AXIS2_HOME%/lib/commons-httpclient-3.1.jar;%AXIS2_HOME%/lib/commons-logging-1.1.1.jar;%AXIS2_HOME%/lib/httpcore-4.0.jar;%AXIS2_HOME%/lib/mail-1.4.jar;%AXIS2_HOME%/lib/neethi-3.0.2.jar;%AXIS2_HOME%/lib/wsdl4j-1.6.2.jar;%AXIS2_HOME%/lib/XmlSchema-1.4.7.jar
+java %CP% com.ibm.greenhat.examples.calculator.Calculator http://localhost:7819/RTCP
 
 endlocal
 popd
